1d513debb93acb1759875a852d00b190a235a7df
Balazs Komuves <bkomuves@gmail.com>
2024-12-13 20:20:43 +0100

    move source code into an `src` subdirectory

--- src/testmain.hs
+++ src/testmain.hs
@@ -21,14 +21,14 @@ main = do
   -- let prefix = "recursion_outer"
   let prefix = "lookup"
 
-  text_common <- L.readFile ("json/" ++ prefix ++ "_common.json")
-  text_vkey   <- L.readFile ("json/" ++ prefix ++ "_vkey.json"  )
-  text_proof  <- L.readFile ("json/" ++ prefix ++ "_proof.json" )
+  text_common <- L.readFile ("../json/" ++ prefix ++ "_common.json")
+  text_vkey   <- L.readFile ("../json/" ++ prefix ++ "_vkey.json"  )
+  text_proof  <- L.readFile ("../json/" ++ prefix ++ "_proof.json" )
 
   let Just common_data   = decode text_common :: Maybe CommonCircuitData
   let Just verifier_data = decode text_vkey   :: Maybe VerifierOnlyCircuitData
   let Just proof_data    = decode text_proof  :: Maybe ProofWithPublicInputs
-
+  
   let challenges = proofChallenges common_data verifier_data proof_data
 
   print challenges
